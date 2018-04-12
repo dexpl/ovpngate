74257eb1fcd2800ed2717508710db6de21e5dc54
Vadim Raskhozhev <iamdexpl@gmail.com>
2018-04-13 02:33:21 +0300

    bin/getvpngateconfs.sh: added additional checking the servers' origin country via geoiplookup

--- bin/getvpngateconfs.sh
+++ bin/getvpngateconfs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 country=${1:-ru}
-confDir="${2:-$(pwd)}"
+confDir="${2:-.}"
 curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf ; do echo ${host} >&2 ; echo ${conf} | base64 -di > "${confDir}/vpngate_${host}.conf" ; done
+# Try to check whether remote servers actually reside in given country and eliminate those which don't
+# TODO haven't thoroughly tested
+which geoiplookup || exit
+awk '$1 == "remote" { print(FILENAME, $2) }' "${confDir}/vpngate*.conf | while read file ip ; do geoiplookup ${ip} | grep -qwe RU || rm -f ${file} ; done
