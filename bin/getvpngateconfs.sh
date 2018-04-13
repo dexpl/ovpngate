fe2be52c47f6cbb914da91ef466060b7be7cc173
Vadim Raskhozhev <iamdexpl@gmail.com>
2018-04-13 09:51:47 +0300

    bin/getvpngateconfs.sh: a bit more improved geoiplookup check

--- bin/getvpngateconfs.sh
+++ bin/getvpngateconfs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -o pipefail
 country=${1:-ru}
 confDir="${2:-.}"
 curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf ; do echo ${host} >&2 ; echo ${conf} | base64 -di > "${confDir}/vpngate_${host}.conf" ; done
