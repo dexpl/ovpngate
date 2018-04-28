b4f71528609fcadc8c63b1fc6fdd64add5becf74
Vadim Raskhozhev <iamdexpl@gmail.com>
2018-04-28 17:10:13 +0300

    vpngate: a bit improved country setting

--- bin/getvpngateconfs.sh
+++ bin/getvpngateconfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -o pipefail
-country=${1:-ru}
+country=${country:-{1:-ru}}
 confDir="${2:-.}"
 curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf ; do echo ${host} >&2 ; echo ${conf} | base64 -di > "${confDir}/vpngate_${host}.conf" ; done
 # Try to check whether remote servers actually reside in given country and eliminate those which don't
