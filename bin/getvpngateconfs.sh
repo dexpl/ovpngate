f869509329ae00dbe46f62288e93b9022be3d781
Vadim Raskhozhev <iamdexpl@gmail.com>
2019-04-02 13:41:17 +0300

    bin/getvpngateconfs.sh: fixed a silly bug

--- bin/getvpngateconfs.sh
+++ bin/getvpngateconfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -o pipefail
-country=${country:-{1:-ru}}
+country=${country:-${1:-ru}}
 confDir="${2:-.}"
 curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf
 do
