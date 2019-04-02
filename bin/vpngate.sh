090903c53cf033c0fd8d8e572baf0f7b4349a010
Vadim Raskhozhev <iamdexpl@gmail.com>
2019-04-02 13:04:04 +0300

    bin/vpngate.sh: a little bugfix

--- bin/vpngate.sh
+++ bin/vpngate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 confFile=vpngate.conf
-rm "${confFile}"
+[ -e "${confFile}" ] && rm "${confFile}"
 stat -c %n *.conf  || getvpngateconfs.sh
 find . -name '*.conf' -type f | tail -n 1 | xargs -I % mv -v % "${confFile}"
