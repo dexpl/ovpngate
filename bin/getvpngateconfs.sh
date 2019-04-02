10e8778eafd809951b805fb174dc1f650aa66632
Vadim Raskhozhev <iamdexpl@gmail.com>
2019-04-02 13:23:32 +0300

    bin/getvpngateconfs.sh: reformatted

--- bin/getvpngateconfs.sh
+++ bin/getvpngateconfs.sh
@@ -3,7 +3,11 @@
 set -o pipefail
 country=${country:-{1:-ru}}
 confDir="${2:-.}"
-curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf ; do echo ${host} >&2 ; echo ${conf} | base64 -di > "${confDir}/vpngate_${host}.conf" ; done
+curl -s http://www.vpngate.net/api/iphone/ | awk -F , -vco=${country} '$7 == toupper(co) { print($1, $NF) }' | while read host conf
+do
+	echo ${host} >&2
+	echo ${conf} | base64 -di > "${confDir}/vpngate_${host}.conf"
+done
 # Try to check whether remote servers actually reside in given country and eliminate those which don't
 # TODO haven't thoroughly tested
 which geoiplookup > /dev/null || exit
