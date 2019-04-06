0d2b040655d945a916b74d0e2b6d26f35559059d
Vadim Raskhozhev <iamdexpl@gmail.com>
2019-04-06 23:27:35 +0300

    bin/vpngate.sh: integrated with getvpngateconfs.pl

--- bin/vpngate.sh
+++ bin/vpngate.sh
@@ -2,8 +2,10 @@
 
 # confFile=vpngate.conf if $countries is unset, vpngate-${countries}.conf
 # otherwise
-confDir=vpngate${countries:+-${countries}}
-confFile=${confDir}.conf
-[ -e "${confFile}" ] && rm "${confFile}"
-stat -c %n *.conf  || getvpngateconfs.pl
-find ${confDir} -name '*.conf' -type f | shuf -n 1 | xargs -I % mv -v % "${confFile}"
+confDir="vpngate${countries:+-${countries}}"
+confFile="${confDir}.conf"
+until [ -n "${newConfFile}" ]; do
+	newConfFile=$(find "${confDir}" -name '*.conf' -type f | shuf -n 1)
+	[ -z "${newConfFile}" ] && getvpngateconfs.pl "${countries}" "${confDir}"
+done
+mv -v "${newConfFile}" "${confFile}"
